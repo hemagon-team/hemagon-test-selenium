b59f60ff11f3c5ed45c5a01b7c7f929d1b9e37d1
Nikolai Morozov <avanmorozov@gmail.com>
2024-04-07 23:59:56 +0400

    allow non docker usage

--- conftest.py
+++ conftest.py
@@ -1,7 +1,13 @@
 import os
 import pytest
+from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as OptionsChrome
+from selenium.webdriver.chrome.service import Service as ChromiumService
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.os_manager import ChromeType
+
+load_dotenv()
 
 
 @pytest.fixture(scope="function")
@@ -10,7 +16,10 @@ def browser(request):
     options_chrome.add_argument("--window-size=1920,1080")
     options_chrome.add_argument("--headless")
     print("\nstart chrome browser for test...")
-    browser = webdriver.Remote(os.environ["SELENIUM_HUB_URL"], options=options_chrome)
+    if os.environ["DEV_ENV_MODE"] == 'remote':
+        browser = webdriver.Remote(os.environ["SELENIUM_HUB_URL"], options=options_chrome)
+    else:
+        browser = webdriver.Chrome(service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()), options=options_chrome)
     yield browser
     print("\nquit browser...")
     browser.quit()
